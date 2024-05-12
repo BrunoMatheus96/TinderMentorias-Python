7eea6708883ed2017ef299d085a71f0acb28647c
Bruno Matheus Stefano Leite <105085425+BrunoMatheus96@users.noreply.github.com>
2024-05-11 21:40:06 -0300

    Ajustes de Swagger e rota

--- src/auth/AuthController.py
+++ src/auth/AuthController.py
@@ -14,29 +14,27 @@ authService = AuthService()
 userService = UserService()
 
 
-@router.post('/login', responses={
-    200: {"model": str, "description": "Login realizado com sucesso",
+@router.post('/login', tags=['Autenticação'], responses={
+    200: {"model": str, "description": "Mensagem sucesso",
           "content": {"application/json": {"example": {"detail": "string", "dados": "string", "status": 200}}}},
     401: {"model": str, "description": "Usuário não autorizado",
           "content": {"application/json": {"example": {"detail": "string"}}}}
 })
-async def login_de_usuario(login_dto: LoginDTO = Body(...)):
+async def login(login_dto: LoginDTO = Body(...)):
     result = await authService.login(login_dto)
 
     # Retorna o que foi feito no auth.AuthService.py SE os dados enviados forem do tipo ao do auth.dto
     return result
 
 
-@router.post('/register', status_code=201, responses={
-    201: {"model": str, "description": "Login realizado com sucesso",
+@router.post('/register', status_code=201, tags=['Autenticação'], responses={
+    201: {"model": str, "description": "Mensagem sucesso",
           "content": {"application/json": {"example": {"detail": "string"}}}},
     400: {"model": str, "description": "Usuário não autorizado",
           "content": {"application/json": {"example": {"detail": "string"}}}}
 })
 async def cadastro_de_usuario(file: UploadFile, register_dto: RegisterDTO = Depends(RegisterDTO)):
     try:
-        print(file)
-        print('.jpg' in file.filename)
 
         if not ('.jpg' in file.filename or '.jpeg' in file.filename or '.png' in file.filename):
             raise HTTPException(400, 'Formato do arquivo incorreto')
---
+++ src/user/UserController.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends, Header
+
+from src.auth.AuthService import AuthService
+from src.core.middlewares.JWTMiddleware import check_token
+from src.user.UserService import UserService
+
+router = APIRouter()
+
+user = UserService()
+authService = AuthService()
+userService = UserService()
+
+
+@router.put('/upload_user', tags=['Usuários'])
+async def atualizar_dados_do_usuario():
+
+    return 'Ta funcionando'
